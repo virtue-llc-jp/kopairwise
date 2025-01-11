735bf6384ab1d48dd56777dd4200f81f7970038e
KIKUCHI Shunsuke <s-kikuchi@virtualex.co.jp>
2025-01-11 22:46:52 +0900

    publishing script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export SONATYPE_CENTRAL_USERNAME='virtue-llc-jp'
+export PGP_SIGNING_KEY=$(gpg --armor --export-secret-keys info@virtue.llc)
+
+echo -n "SonarType Central Password: "
+read -s SONATYPE_CENTRAL_PASSWORD
+echo
+
+echo -n "PGP signing key PassPhrase: "
+read -s PGP_SIGNING_KEY_PASSPHRASE
+echo
+
+./gradlew sonatypeCentralUpload
